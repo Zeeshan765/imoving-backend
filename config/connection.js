d12957e02c29621904bd9061ac8aea416aa738e5
Zeeshan Ashraf <zeeshanashraf765@gmail.com>
2024-10-15 00:06:18 +0500

    railway file added

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const dbConnection = async () => {
   try {
     const connected = await mongoose.connect(
+      `${process.env.DATABASE_URI}`,
       // `mongodb://127.0.0.1:27017/${process.env.DATABASE_NAME}`,
-      'mongodb+srv://shakir:devshakir1234$@cluster0.eq7ax.mongodb.net/movie-app?retryWrites=true&w=majority&appName=Cluster0'
+      // 'mongodb+srv://shakir:devshakir1234$@cluster0.eq7ax.mongodb.net/movie-app?retryWrites=true&w=majority&appName=Cluster0'
     );
+
 
     if (connected) {
       // console.log(`Connected to the MongoDB : ${process.env.DATABASE_NAME}`);
@@ -15,9 +17,7 @@ const dbConnection = async () => {
       //   `Failed to connect with MongoDB : ${process.env.DATABASE_NAME}`,
 
       // );
-      throw new Error(
-        `Failed to connect with MongoDB : immoving`,
-      )
+      throw new Error(`Failed to connect with MongoDB : immoving`);
     }
 
     return true;
@@ -25,9 +25,7 @@ const dbConnection = async () => {
     // throw new Error(
     //   `Failed to connect with MongoDB : ${process.env.DATABASE_NAME}`,
     // );
-    throw new Error(
-      `Failed to connect with MongoDB : immoving`,
-    )
+    throw new Error(`Failed to connect with MongoDB : immoving`);
   }
 };
 
