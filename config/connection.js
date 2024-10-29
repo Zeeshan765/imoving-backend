49a7fa10c9eb43c782894d0ca289d6c7ec5b2493
Zeeshan Ashraf <zeeshanashraf765@gmail.com>
2024-10-30 00:20:42 +0500

    chnages

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,15 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    console.log('process.env.DATABASE_NAME', process.env.DATABASE_NAME)
     const connected = await mongoose.connect(
-      // `${process.env.DATABASE_URI}`,
-      `mongodb://127.0.0.1:27017/${process.env.DATABASE_NAME}`,
+      `${process.env.DATABASE_URI}`,
+      // `mongodb://127.0.0.1:27017/${process.env.DATABASE_NAME}`,
       // 'mongodb+srv://shakir:devshakir1234$@cluster0.eq7ax.mongodb.net/movie-app?retryWrites=true&w=majority&appName=Cluster0'
     );
 
     if (connected) {
       // console.log(`Connected to the MongoDB : ${process.env.DATABASE_NAME}`);
-      console.log(`Connected to the MongoDB :${process.env.DATABASE_NAME}`);
+      console.log(`Connected to the MongoDB :immoving`);
     } else {
       // throw new Error(
       //   `Failed to connect with MongoDB : ${process.env.DATABASE_NAME}`,
